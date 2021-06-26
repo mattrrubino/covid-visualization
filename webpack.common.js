61fd8320f3359b1077693b511d9f2ad40c1069d1
Matthew Rubino <mrrubino@buffalo.edu>
2021-06-26 11:01:16 -0400

    Add development server

--- webpack.common.js
+++ webpack.common.js
@@ -1,13 +1,7 @@
-const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'production',
     entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.[contenthash].js'
-    },
     plugins: [
         new HtmlWebpackPlugin({
             template: "./src/template.html"
